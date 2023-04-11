ef8b4adf7eaa83931482d3e559b71f1780f09c55
KKB5 <karthikeyan32b@gmail.com>
2023-04-11 16:43:11 +0530

    second branch commit

--- frontend/index.js
+++ frontend/index.js
@@ -8,31 +8,24 @@ const check = () => {
 }
 
 const registerServiceWorker = async () => {
-    const swRegistration = await navigator.serviceWorker.register('service.js');
-    return swRegistration;
+    const swRegistration = await navigator.serviceWorker.register('service.js')
+    return swRegistration
 }
 
-
 const requestNotificationPermission = async () => {
-    const permission = await window.Notification.requestPermission();
+    const permission = await window.Notification.requestPermission()
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
     if (permission !== 'granted') {
-        throw new Error('Permission not granted for Notification');
+        throw new Error('Permission not granted for Notification')
     }
 }
 
-const showLocalNotification = (title, body, swRegistration) => {
-    const options = {
-        body,
-    };
-    swRegistration.showNotification(title, options);
-
-}
-
 const main = async () => {
-    check();
-    const swRegistration = await registerServiceWorker();
-    const permission = await requestNotificationPermission();
-    showLocalNotification('Test push notiification', 'message to test', swRegistration);
+    check()
+    const swRegistration = await registerServiceWorker()
+    const permission = await requestNotificationPermission()
 }
-
-main();
+  // main(); we will not call main in the beginning.
--- frontend/service.js
+++ frontend/service.js
@@ -1,4 +1,24 @@
+const urlB64ToUint8Array = (base64String) => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray;
+}
+
 self.addEventListener('activate', async () => {
     // This will be called only once when the service worker is activated.
-    console.log('service worker activate')
+    try {
+        const applicationServerKey = urlB64ToUint8Array(
+            'BFuR9HeG0NorXJbBQZdIj3Z7euYpwe30T84wrkNo2SLh0rQINiJwhVfHqFvpsC4EocQRmekamvVWMTE-r7N9w6Y'
+        )
+        const options = { applicationServerKey, userVisibleOnly: true }
+        const subscription = await self.registration.pushManager.subscribe(options)
+        console.log(JSON.stringify(subscription))
+    } catch (err) {
+        console.log('Error', err);
+    }
 })
